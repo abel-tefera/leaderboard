155331d2bed80cf572f010e1ac73a1fe3a8534c6
abel-tefera <abeltefera16@gmail.com>
2023-04-12 10:22:40 +0300

    Sorting added for results

--- src/api/getScores.js
+++ src/api/getScores.js
@@ -4,6 +4,13 @@ export const getScores = async () => {
   try {
     const response = await fetch(`${BASE_URL}/games/${GAME_ID}/scores`);
     const { result } = await response.json();
+
+    result.sort((a, b) => {
+      const x = parseInt(a.score);
+      const y = parseInt(b.score);
+      return x < y ? 1 : x > y ? -1 : 0;
+    });
+
     return result;
   } catch (error) {
     console.error(error);
